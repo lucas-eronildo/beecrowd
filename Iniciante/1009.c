16213b620f998c1dbd9855d302cd370dae9f4517
lucas-eronildo <lucaseronildogrog2023@gmail.com>
2026-06-07 21:34:02 -0300

    questao 10

---
+++ Iniciante/1009.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    char nome[50];
+    float salario, vendas;
+    fgets(nome, sizeof(nome), stdin);
+    scanf("%f %f", &salario, &vendas);
+    printf("TOTAL = %.2f", salario + (vendas*0.15));
+}
