4afa882d85bc314d521ae8db41dc8179dad25d99
lucas-eronildo <lucaseronildogrog2023@gmail.com>
2026-06-18 20:38:37 -0300

    b

---
+++ Iniciante/1189.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    double M[12][12], soma = 0;
+    char op;
+    scanf(" %c", &op);
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            scanf("%lf", &M[i][j]);
+        }
+    }
+    for(int i = 0; i < 5; i++){
+        for(int j = 1 + i; j < 11 - i; j++){
+            soma += M[j][i];
+        }
+    }
+    switch(op){
+        case 'S':
+            printf("%.1lf\n", soma);
+            break;
+        case 'M':
+            printf("%.1lf\n", soma/30.0);
+            break;
+    }
+}
